fd7e8be8518e4883d162e80be0b24e4612ccc317
Tracy H <aag9n17o@duck.com>
2023-07-24 10:24:44 -0500

    initial commit - test allhubs repo

--- scriptHelper.js
+++ scriptHelper.js
@@ -41,3 +41,16 @@ module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
+
+/*ONLY MODIFY THIS AND SCRIPT.JS*/
+
+/*
+Adding Validation
+Adding Alerts
+
+ In scriptHelper.js, you will use validateInput() to complete the formSubmission() function. formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass. Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below. Make sure to call your formSubmission() function at the appropriate time in your script.js file!
+
+Note
+
+If you want to check if something is NaN, you cannot use == or ===. Instead, JavaScript has a built-in method called isNaN(value) that returns true if value is NaN and false if value is not NaN.
+*/
